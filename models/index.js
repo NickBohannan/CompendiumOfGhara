be2086d9d9d74bbec7d570d3dc816215e41bf313
Nick Bohannan <nbohannan@gmail.com>
2018-09-20 17:22:20 -0500

    changed db var

--- models/index.js
+++ models/index.js
@@ -4,13 +4,7 @@ let db
 
 if (process.env.DATABASE_URL) {
     // the application is executed on Heroku ... use the postgres database
-    db = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        port: match[4],
-        host: match[3],
-        logging: true //false
-    })
+    db = new Sequelize(process.env.DATABASE_URL)
 }
 
 const Entry = db.define("entry", {
