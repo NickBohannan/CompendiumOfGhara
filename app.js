e328659b36629a134ab027ccf828b22e07d63be0
Nick Bohannan <nbohannan@gmail.com>
2018-09-20 19:05:30 -0500

    port var changed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'))
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.set('port', port)
 app.set('view engine', 'ejs')
